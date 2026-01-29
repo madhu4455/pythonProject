5edb5623e309d6ad43f04f95adc82c22838417d3
madhu4455 <madhu4455@gmail.com>
2026-01-29 11:59:47 +0530

    (New changes modified for file sample.py)

--- SeleniumTest/sample.py
+++ SeleniumTest/sample.py
@@ -1,1 +1,13 @@
 Execute all the test cases and update the test results in the metrix document.
+class A:
+    def__(init)__(self):
+        self.ename = ename
+        self.eid = eid
+        self.esalary = esalary
+
+e1 = A()
+e1.ename
+e1.eid
+e1.esalary
+
+print(ename,eid,esalary)
